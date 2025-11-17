c01f64c5de0585a6d7a250cd2887c04e006b1cf9
mathis.constant <mathis.constant@epita.fr>
2025-11-17 22:12:41 +0100

    Début code pygame

--- game.py
+++ game.py
@@ -1,3 +1,22 @@
-# Contient le code du jeu
+import pygame #importation de pygame
+from pygame.locals import *
 
-import pygame
+pygame.init() #démarrage de pygame
+pygame.mixer.init()
+
+"""
+Ajout des variables à mettre ici
+
+"""
+
+#création d'une fenêtre
+fenetre=pygame.display.set_mode((1920,1080))#fenêtre de taille 1920*1080
+
+
+
+#chargement des images
+liste_actuelle=[]
+
+#images de fond (menus et maps)
+menu = pygame.image.load("menu.png").convert_alpha()
+menu = pygame.transform.scale(menu,(1920,1080))
