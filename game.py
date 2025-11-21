0c8a2cdd1dcb7515ef99eb2f79a9cc54a8698a38
GrosBiscuits <redtzimmerquentin@gmail.com>
2025-11-21 20:04:04 +0100

    Débuts def tuiles

--- game.py
+++ game.py
@@ -1,3 +1,26 @@
 # Contient le code du jeu
-
+ 
 import pygame
+
+# Tuiles de terrain (à compléter avec les bons chemins d'accès aux images)
+
+plaine_tuile = pygame.image.load("")
+
+mountain_tuile = pygame.image.load("")
+
+water_tuile = pygame.image.load("")
+
+forest_tuile = pygame.image.load("")
+
+snow_plaine_tuile = pygame.image.load("")
+
+snow_forest_tuile = pygame.image.load("")
+
+snow_mountain_tuile = pygame.image.load("")
+
+ice_tuile = pygame.image.load("")
+
+desert_tuile = pygame.image.load("")
+
+desert_mountain_tuile = pygame.image.load("")
+
