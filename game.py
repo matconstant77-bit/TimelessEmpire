6fcfb689b82f7d8cedea8d49ce8ea828005b74a2
GrosBiscuits <redtzimmerquentin@gmail.com>
2025-11-27 18:20:23 +0100

    Class tuiles

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 # Contient le code du jeu
+
 import pygame #importation de pygame
 from pygame.locals import *
 
@@ -12,25 +13,31 @@ Ajout des variables à mettre ici
 
 # Tuiles de terrain (à compléter avec les bons chemins d'accès aux images)
 
-plaine_tuile = pygame.image.load("")
+tuiles = {
+    'mountain_grass': pygame.image.load(""),
+    'mountain_forest': pygame.image.load(""),
+    'mountain_snow': pygame.image.load(""),
+    'mountain_sand': pygame.image.load(""),
+    'plain_grass': pygame.image.load(""),
+    'plain_forest': pygame.image.load(""),
+    'plain_snow': pygame.image.load(""),
+    'desert': pygame.image.load(""),
+    'forest': pygame.image.load(""),
+    'snow_forest': pygame.image.load(""),
+    'water': pygame.image.load(""),
+}
 
-mountain_tuile = pygame.image.load("")
+# création d'une classe Hexagone pour créer la map
 
-water_tuile = pygame.image.load("")
+class Hexagone:
+    def __init__(self, q, r, type_terrain):
+        self.q = q                           # Coordonnée q
+        self.r = r                           # Coordonnée r
+        self.type_terrain = type_terrain     # 'herbe', 'foret', 'eau', etc.
+        self.tuile = tuiles[type_terrain]   # L'image PNG associée
 
-forest_tuile = pygame.image.load("")
 
-snow_plaine_tuile = pygame.image.load("")
 
-snow_forest_tuile = pygame.image.load("")
-
-snow_mountain_tuile = pygame.image.load("")
-
-ice_tuile = pygame.image.load("")
-
-desert_tuile = pygame.image.load("")
-
-desert_mountain_tuile = pygame.image.load("")
 
 #création d'une fenêtre
 fenetre=pygame.display.set_mode((1920,1080))#fenêtre de taille 1920*1080
@@ -44,3 +51,7 @@ liste_actuelle=[]
 menu = pygame.image.load("menu.png").convert_alpha()
 menu = pygame.transform.scale(menu,(1920,1080))
 >>>>>>> c01f64c5de0585a6d7a250cd2887c04e006b1cf9
+
+
+
+
